4ae890bf603a83682ee7d544eed407ccd7161b91
Luffy Zhao <luffhzhao@vip.126.com>
2018-12-12 17:34:34 +0800

    更改提示模板

--- src/Util/MarkdownBuild.php
+++ src/Util/MarkdownBuild.php
@@ -47,7 +47,7 @@ class MarkdownBuild
     {
         return [
             '{{ http_url }}' => $this->isHttps() ? 'https' : 'http' . '://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"],
-            '{{ get }}' => $_SERVER["REQUEST_URI"],
+            '{{ get }}' => $_SERVER["QUERY_STRING"],
             '{{ post }}' => $this->arrayToString($_POST),
         ];
     }
